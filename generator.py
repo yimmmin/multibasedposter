6123f33e783bfc2c5ca3dcc06713a254c9801b4a
yimmmin <46152975+yimmmin@users.noreply.github.com>
2019-01-29 22:16:32 +0800

    note addition

--- generator.py
+++ generator.py
@@ -1,5 +1,5 @@
 import baidu_pic
 import postergenerator
 
-# baidu_pic.baidu_picget('新垣结衣',30)
-postergenerator.poster_generator(200,200,5)
+baidu_pic.baidu_picget('新垣结衣',30) # 关键字，页数（每页30张），图片文件夹名
+postergenerator.poster_generator(200,200,5) # 目标图片参考像素高，宽，block大小
